dc4345cb2569b64c9a84020cd9f111bfa5bd7a1f
R. Tyler Croy <rtyler@brokenco.de>
2019-03-09 22:23:10 -0800

    Allow the directories to be built independently

--- common.mk
+++ common.mk
@@ -1,3 +1,16 @@
+#
+## The following variables are defined to allow building a single directory
+## without running the whole Makefile infrastructure, e.g. `make -C node`
+#
+# GROUP can be overridden in the environment to root the docker images under
+# different registry namespace
+GROUP?=jenkins
+# PREFIX defaults to `jnlp-agent` and can be changed to compute different image
+# names
+PREFIX?=jnlp-agent
+# SUFFIX defaults to the directory name
+SUFFIX?=$(shell basename $(shell pwd))
+
 NAME=$(GROUP)/$(PREFIX)-$(SUFFIX)
 
 build:
