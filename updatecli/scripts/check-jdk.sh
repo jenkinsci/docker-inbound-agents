4ac6b1e4a203d050c1bb08b85908bd3d7a25c155
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-12-05 12:26:36 +0100

    chore(Updatecli): create manifests to track JDKs versions and `jenkins/inbound-agent` version (#48)
    * chore(updatecli): add jdk 8, 17 and 19
    
    * chore(arg): add the jdk11 image as argument for dockerfile
    
    * add updatecli manifest for jdk11 dockerinboundagent image
    
    * add a CST file for jdk11
    
    * ARG position or quotes
    
    * move ARG on top and add quotes

---
+++ updatecli/scripts/check-jdk.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# This script checks that the provided JDK version has all the required binaries available for downloads
+# as sometimes, the tag for adoptium is published immeidatly while binaries are published later.
+##
+# The source of truth is the ERB template stored at the location dist/profile/templates/buildmaster/casc/tools.yaml.erb
+# It lists all the installations used as "Jenkins Tools" by the Jenkins controllers of the infrastructure
+##
+set -eu -o pipefail
+
+command -v curl >/dev/null 2>&1 || { echo "ERROR: curl command not found. Exiting."; exit 1; }
+
+function get_jdk_download_url() {
+  jdk_version="${1}"
+  platform="${2}"
+  case "${jdk_version}" in
+    8*)
+      ## JDK8 does not have the carret ('-') in their archive names
+      echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk${jdk_version}/OpenJDK8U-jdk_${platform}_hotspot_${jdk_version//-}";
+      return 0;;
+    11*)
+      ## JDK11 URLs have an underscore ('_') instead of a plus ('+') in their archive names
+      echo "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-${jdk_version}/OpenJDK11U-jdk_${platform}_hotspot_${jdk_version//+/_}";
+      return 0;;
+    17*)
+      ## JDK17 URLs have an underscore ('_') instead of a plus ('+') in their archive names
+      echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-${jdk_version}/OpenJDK17U-jdk_${platform}_hotspot_${jdk_version//+/_}";
+      return 0;;
+    19*)
+      ## JDK19 URLs have an underscore ('_') instead of a plus ('+') in their archive names
+      echo "https://github.com/adoptium/temurin19-binaries/releases/download/jdk-${jdk_version}/OpenJDK19U-jdk_${platform}_hotspot_${jdk_version//+/_}";
+      return 0;;
+    *)
+      echo "ERROR: unsupported JDK version (${jdk_version}).";
+      exit 1;;
+  esac
+}
+
+platforms=("x64_linux" "x64_windows" "aarch64_linux")
+
+for platform in "${platforms[@]}"
+do
+  url_to_check="$(get_jdk_download_url "${1}" "${platform}")"
+  if [[ "${platform}" == *windows* ]]
+  then
+    url_to_check+=".zip"
+  else
+    url_to_check+=".tar.gz"
+  fi
+  >&2 curl --connect-timeout 5 --location --head --fail --silent "${url_to_check}" || { echo "ERROR: the following URL is NOT available: ${url_to_check}."; exit 1; }
+done
+
+echo "OK: all JDK URL for version=${1} are available."
